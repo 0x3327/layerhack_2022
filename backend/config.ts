83efd5847207c4c569f2a686741b27b88ae248d4
Uros Kukic <>
2022-11-08 16:46:58 +0100

    Add config files

--- backend/config.ts
+++ backend/config.ts
@@ -7,8 +7,9 @@ interface ENV {
     ALTERNATE_PRIVATE_KEY: string | undefined;
 
     AA_FACTORY_ADDRESS: string | undefined;
-    MULTISIG_ADDRESS: string | undefined;
+    ACCOUNT_ADDRESS: string | undefined;
     GREETER_ADDRESS: string | undefined;
+    PLUGIN_ADDRESS: string | undefined;
 
     API_KEY: string | undefined;
     API_ENDPOINT: string | undefined;
@@ -20,8 +21,9 @@ interface Config {
     ALTERNATE_PRIVATE_KEY: string;
 
     AA_FACTORY_ADDRESS: string;
-    MULTISIG_ADDRESS: string;
+    ACCOUNT_ADDRESS: string;
     GREETER_ADDRESS: string;
+    PLUGIN_ADDRESS: string;
 
     API_KEY: string;
     API_ENDPOINT: string;
@@ -36,8 +38,9 @@ const getConfig = (): ENV => {
         ALTERNATE_PRIVATE_KEY: process.env.ALTERNATE_PRIVATE_KEY ? String(process.env.ALTERNATE_PRIVATE_KEY) : undefined,
 
         AA_FACTORY_ADDRESS: process.env.AA_FACTORY_ADDRESS ? String(process.env.AA_FACTORY_ADDRESS) : undefined,
-        MULTISIG_ADDRESS: process.env.MULTISIG_ADDRESS ? String(process.env.MULTISIG_ADDRESS) : undefined,
+        ACCOUNT_ADDRESS: process.env.ACCOUNT_ADDRESS ? String(process.env.ACCOUNT_ADDRESS) : undefined,
         GREETER_ADDRESS: process.env.GREETER_ADDRESS ? String(process.env.GREETER_ADDRESS) : undefined,
+        PLUGIN_ADDRESS: process.env.PLUGIN_ADDRESS ? String(process.env.PLUGIN_ADDRESS) : undefined,
         
         API_KEY: process.env.API_KEY ? String(process.env.API_KEY) : undefined,
         API_ENDPOINT: process.env.API_ENDPOINT ? String(process.env.API_ENDPOINT) : undefined,
