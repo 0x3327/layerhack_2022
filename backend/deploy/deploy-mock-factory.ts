a531f6f91e512150dd53de555ea27202ad3d9104
Uros Kukic <>
2022-11-08 16:50:11 +0100

    Add mock account SC and mock account and mock factory deployment scripts

---
+++ backend/deploy/deploy-mock-factory.ts
@@ -0,0 +1,34 @@
+import { utils, Wallet } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+import sanitizedConfig from "../config";
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+    const wallet = new Wallet(sanitizedConfig.PRIVATE_KEY);
+    const deployer = new Deployer(hre, wallet);
+    const factoryArtifact = await deployer.loadArtifact("AAFactory");
+    const aaArtifact = await deployer.loadArtifact("MockAccount");
+
+    // // Deposit some funds to L2 in order to be able to perform L2 transactions.
+    // // You can remove the depositing step if the `wallet` has enough funds on zkSync
+    // const depositAmount = ethers.utils.parseEther("0.3");
+    // const depositHandle = await deployer.zkWallet.deposit({
+    //     to: deployer.zkWallet.address,
+    //     token: utils.ETH_ADDRESS,
+    //     amount: depositAmount,
+    // });
+    // await depositHandle.wait();
+
+    // Getting the bytecodeHash of the account
+    const bytecodeHash = utils.hashBytecode(aaArtifact.bytecode);
+
+    const factory = await deployer.deploy(factoryArtifact, [bytecodeHash], undefined, [
+        // Since the factory requires the code of the multisig to be available,
+        // we should pass it here as well.
+        aaArtifact.bytecode,
+    ]);
+
+    console.log(`Mock AA factory address: ${factory.address}`);
+}
